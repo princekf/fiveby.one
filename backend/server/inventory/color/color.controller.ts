e6829d5ea575a4e18ecc9261704a0584f407864c
Amith <amithp45@gmail.com>
2020-06-08 13:57:57 +0530

    API changes Brand Color Location

--- backend/server/inventory/color/color.controller.ts
+++ backend/server/inventory/color/color.controller.ts
@@ -1,20 +1,26 @@
 import {Router as expressRouter} from 'express';
-import { authorize } from '../../passport-util';
-import Product from '../product/product.model';
+import {ProductModel} from '../product/product.model';
 import {Constants} from 'fivebyone';
+import { AuthUtil } from '../../util/auth.util';
 
-const {HTTP_OK} = Constants;
+const {HTTP_OK, HTTP_UNAUTHORIZED} = Constants;
 
 const router = expressRouter();
 
 const listColors = async(_request: any, response: any) => {
+  const sessionDetails = AuthUtil.findSessionDetails(_request);
+  if (!sessionDetails.company) {
 
+    return response.status(HTTP_UNAUTHORIZED).json('Permission denied.');
+
+  }
+  const Product = ProductModel.createModel(sessionDetails.company);
   const colors = await Product.distinct('colors');
   return response.status(HTTP_OK).json(colors.filter(Boolean));
 
 };
 
-router.route('/').get(authorize, listColors);
+router.route('/').get(AuthUtil.authorize, listColors);
 
 export default router;
 
--- backend/server/inventory/location/location.controller.ts
+++ backend/server/inventory/location/location.controller.ts
@@ -1,20 +1,26 @@
 import {Router as expressRouter} from 'express';
-import { authorize } from '../../passport-util';
-import Product from '../product/product.model';
+import {ProductModel} from '../product/product.model';
 import {Constants} from 'fivebyone';
+import { AuthUtil } from '../../util/auth.util';
 
-const {HTTP_OK} = Constants;
+const {HTTP_OK, HTTP_UNAUTHORIZED} = Constants;
 
 const router = expressRouter();
 
 const listLocations = async(_request: any, response: any) => {
+  const sessionDetails = AuthUtil.findSessionDetails(_request);
+  if (!sessionDetails.company) {
 
+    return response.status(HTTP_UNAUTHORIZED).json('Permission denied.');
+
+  }
+  const Product = ProductModel.createModel(sessionDetails.company);
   const locations = await Product.distinct('location');
   return response.status(HTTP_OK).json(locations);
 
 };
 
-router.route('/').get(authorize, listLocations);
+router.route('/').get(AuthUtil.authorize, listLocations);
 
 export default router;
 
